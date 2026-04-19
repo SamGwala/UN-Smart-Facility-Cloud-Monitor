3be9b7c447d1935722d6d0e114c2515b2882e9ba
SamGwala <138815066+SamGwala@users.noreply.github.com>
2026-04-19 19:50:47 +0200

    chore: remove duplicate root terraform folder

--- terraform/backend.tf
+++
@@ -1,9 +0,0 @@
-terraform {
-  backend "s3" {
-    bucket         = "smart-cloud-monitor-terrafrom-state"
-    key            = "global/smart-facility/terraform.tfstate"
-    region         = "eu-north-1"
-    dynamodb_table = "terraform-locks"
-    encrypt        = true
-  }
-}
