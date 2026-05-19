edcad9789b301ac7d1ac34829d745c26683229ae
Sam Gwala <138815066+SamGwala@users.noreply.github.com>
2026-05-19 21:38:35 +0200

    Update backend.tf

--- UN-Smart-Facility-Cloud-Monitor/terraform/backend.tf
+++ UN-Smart-Facility-Cloud-Monitor/terraform/backend.tf
@@ -2,7 +2,7 @@ terraform {
   backend "s3" {
     bucket         = "smart-cloud-monitor-terraform-state"
     key            = "global/smart-facility/terraform.tfstate"
-    region         = "eu-east-1"
+    region         = "us-east-1"
     dynamodb_table = "terraform-locks"
     encrypt        = true
   }
